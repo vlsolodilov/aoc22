1b3fd69835984c7d1ca52a2ebd1e0afe2b427294
Солодилов Владислав <vlsolodilov@gmail.com>
2022-12-03 12:40:50 +0300

    day 3

---
+++ src/Day03.kt
@@ -0,0 +1,36 @@
+fun main() {
+
+    fun getEqualItems(rucksack: String): Char {
+        val rucksackItems = rucksack.toList()
+        val rucksackSize = rucksackItems.size
+        val firstCompartment = rucksackItems.subList(0, (rucksackSize + 1) / 2)
+        val secondCompartment = rucksackItems.subList((rucksackSize + 1) / 2, rucksackSize)
+        return firstCompartment.first { item -> secondCompartment.contains(item) }
+    }
+
+    fun getPriorityItem(item: Char): Int {
+        val codeDelta =  if (item.isLowerCase()) 96 else 38
+        return item.code - codeDelta
+    }
+
+    fun part1(input: List<String>): Int =
+        input.map { getEqualItems(it) }.sumOf { getPriorityItem(it) }
+
+    fun getGroupEqualItems(group: List<String>): Char =
+        group[0].toList().first { item ->
+            group[1].toList().contains(item) && group[2].toList().contains(item) }
+
+
+    fun part2(input: List<String>): Int =
+        input.chunked(3).map { getGroupEqualItems(it) }.sumOf { getPriorityItem(it) }
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+}
